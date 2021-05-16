b17ddfce94a1b00e6c7d4ba8af48cfb5b6adaa13
kramlj00 <73222835+kramlj00@users.noreply.github.com>
2021-05-16 10:35:24 +0200

    Adding global styles

--- screens/home.js
+++ screens/home.js
@@ -1,22 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { globalStyles } from '../styles/global'
+
 export default function Home(){
     return(
-        <View style = {styles.container}>
-            <Text style = {styles.titleText}>Home Screen</Text>
+        <View style = {globalStyles.container}>
+            <Text style = {globalStyles.titleText}>Home Screen</Text>
         </View>
     )
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 20,
-    },
-    titleText: {
-        fontFamily: 'nunito-bold',
-        fontSize: 18,
-        color: '#333'
-    },
-})
