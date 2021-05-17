f894e6f50f83cf93507ffd9731e1e5ebdad9cab4
kramlj00 <73222835+kramlj00@users.noreply.github.com>
2021-05-17 17:53:08 +0200

    Adding Card component to reviewDetails.js

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, View, Button} from 'react-native';
 
 import { globalStyles } from '../styles/global'
+import Card from '../shared/card'
 
 // only components in homeStack.js can access navigation prop 
 export default function ReviewDetails({ route }){
@@ -10,9 +11,11 @@ export default function ReviewDetails({ route }){
 
     return(
         <View style = {globalStyles.container}>
-            <Text>{title}</Text>
-            <Text>{body}</Text>
-            <Text>{rating}</Text>
+            <Card>
+                <Text>{title}</Text>
+                <Text>{body}</Text>
+                <Text>{rating}</Text>
+            </Card>
         </View>
     )
 }
